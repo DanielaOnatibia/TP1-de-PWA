510be184cdcb4909f303cfe3b31bf6ce7af88d60
Erick Gonzalez <gempro30yt@gmail.com>
2026-04-06 01:57:54 -0300

    Componente Selector Creado, Agregue a Home.jsx para prueba de componente

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,28 @@
 import styles from "./Home.module.css";
 import Titulo from "../../Components/Titulo/Titulo";
+import SelectorGeneral from "../../Components/SelectorGeneral/SelectorGeneral";
+import { useState } from "react";
 
 export function Home() {
+  const generoPelis = [
+    "Acción",
+    "Comedia",
+    "Drama",
+    "Terror",
+    "Ciencia Ficción",
+    "Documental",
+  ];
+  const [generoElegido, setGeneroElegido] = useState("");
+
   return (
     <div className={styles.container}>
       <Titulo texto="Mi Aplicación PWA" />
-      <h1>Hola mundo</h1>
+      <SelectorGeneral
+        label="Género de la película"
+        options={generoPelis}
+        value={generoElegido}
+        onChange={setGeneroElegido}
+      />
     </div>
   );
 }
